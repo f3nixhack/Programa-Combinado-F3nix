de73a657e62a7abbb9f7458c8b6a9fde05bf60e3
F3nix <50671074+f3nixhack@users.noreply.github.com>
2024-04-19 21:59:37 -0300

    Update main.py
    Agregado limpieza de metadatos

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #F3nix
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import ttk, filedialog
 import subprocess
 import speedtest
 import requests
+import os
 
 # Función para procesar un archivo
 def procesar_archivo(archivo):
@@ -66,6 +67,22 @@ def obtener_ip_y_abrir_mapa():
     except requests.RequestException:
         resultado_label.config(text='Error al obtener la dirección IP.')
 
+# Función para borrar metadatos de un archivo
+def borrar_metadatos(archivo):
+    try:
+        subprocess.run(["exiftool", "-all=", archivo])
+        resultado_label.config(text=f'Se han borrado los metadatos de {archivo}')
+    except FileNotFoundError:
+        resultado_label.config(text='ExifTool no está instalado. Por favor, instálelo para utilizar esta función.')
+
+# Función para elegir archivo para borrar metadatos
+def elegir_archivo_borrar_metadatos():
+    archivo = filedialog.askopenfilename(filetypes=[("Todos los archivos", "*.*")])
+    if archivo:
+        borrar_metadatos_button.config(state="normal")
+        archivos_seleccionados.append(archivo)
+        archivos_seleccionados_label.config(text="Archivo seleccionado para borrar metadatos:\n" + archivo)
+
 # Crear la ventana principal
 ventana = tk.Tk()
 ventana.title("Programa Combinado F3NIX")
@@ -74,31 +91,42 @@ ventana.title("Programa Combinado F3NIX")
 archivos_seleccionados = []
 
 # Botones
-agregar_button = tk.Button(ventana, text="Agregar Archivo", command=agregar_archivo)
+style = ttk.Style()
+style.configure('Boton3D.TButton', relief='raised', borderwidth=5, background='black', foreground='green')
+agregar_button = ttk.Button(ventana, text="Agregar Archivo", command=agregar_archivo, style='Boton3D.TButton')
 agregar_button.pack()
 
-generar_button = tk.Button(ventana, text="Generar Resultado", command=generar_resultado)
+generar_button = ttk.Button(ventana, text="Generar Resultado", command=generar_resultado, style='Boton3D.TButton')
 generar_button.pack()
 
 # Entry para ingresar la contraseña de sudo
-contrasena_entry = tk.Entry(ventana, show="*")
+contrasena_entry = tk.Entry(ventana, show="*", bg='black', fg='green')
 contrasena_entry.pack()
 
-escanear_arp_button = tk.Button(ventana, text="Escanear ARP", command=escanear_arp)
+escanear_arp_button = ttk.Button(ventana, text="Escanear ARP", command=escanear_arp, style='Boton3D.TButton')
 escanear_arp_button.pack()
 
-velocidad_button = tk.Button(ventana, text="Medir Velocidad de Internet", command=medir_velocidad)
+velocidad_button = ttk.Button(ventana, text="Medir Velocidad de Internet", command=medir_velocidad, style='Boton3D.TButton')
 velocidad_button.pack()
 
-mapa_button = tk.Button(ventana, text="Obtener IP y Abrir Mapa", command=obtener_ip_y_abrir_mapa)
+mapa_button = ttk.Button(ventana, text="Obtener IP y Abrir Mapa", command=obtener_ip_y_abrir_mapa, style='Boton3D.TButton')
 mapa_button.pack()
 
+# Botón para elegir archivo para borrar metadatos
+elegir_archivo_borrar_metadatos_button = ttk.Button(ventana, text="Elegir Archivo para Borrar Metadatos", command=elegir_archivo_borrar_metadatos, style='Boton3D.TButton')
+elegir_archivo_borrar_metadatos_button.pack()
+
+# Botón para borrar metadatos
+borrar_metadatos_button = ttk.Button(ventana, text="Borrar Metadatos", command=lambda: borrar_metadatos(archivos_seleccionados[0]), style='Boton3D.TButton', state="disabled")
+borrar_metadatos_button.pack()
+
 # Etiqueta para mostrar los archivos seleccionados
-archivos_seleccionados_label = tk.Label(ventana, text="Archivos seleccionados:")
+archivos_seleccionados_label = tk.Label(ventana, text="Archivos seleccionados:", bg='black', fg='green')
 archivos_seleccionados_label.pack()
 
 # Etiqueta para mostrar el resultado
-resultado_label = tk.Label(ventana, text="")
+resultado_label = tk.Label(ventana, text="", bg='black', fg='green')
 resultado_label.pack()
 
+ventana.configure(bg='black')  # Cambia el color de fondo de la ventana principal
 ventana.mainloop()
